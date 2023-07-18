437994c15e17e8508c644a2260d898957dc538e1
surya <suryavivek85@gmail.com>
2023-07-18 11:29:06 +0530

    t1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+
+  let [disval,setdisval]=useState({display:"none"})
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <button className="model-show" onClick={()=>{setdisval({display:"block"});console.log(disval)}}>Show Modal</button>
+        <div className="model-overlay" style={disval}>
+          <button className="model-close" onClick={()=>{setdisval({display:"none"});console.log(disval)}}>Close</button>
+          <p className="model-p">This is content of the modal</p>
+        </div>
     </div>
   )
 }
